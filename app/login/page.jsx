ae8b4c6cac8b781c93d800995eefd4baae8ceba8
MercutioG <110576904+MercutioG@users.noreply.github.com>
2024-05-01 15:47:10 -0700

    Created Login page
    - Nearly completed login page front end
    - Set up signup css

---
+++ app/login/page.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Login = () => {
+  return (
+    <div className="flex justify-center login-background items-center">
+    {/* Login Box */}
+    <form className="login-container rounded-lg">
+      {/* Logo and header */}
+      <section className="flex items-center login-header p-4 rounded-t-lg">
+        <Image
+          src="https://cdn.pixabay.com/photo/2017/08/12/23/29/background-texture-2635740_640.jpg"
+          width={300}
+          height={300}
+          alt="Paw Logo"
+
+          className="size-24 mx-4"
+        />
+        <p className="text-6xl px-2 text-white">Login</p>
+      </section>
+
+      {/* Login inputs */}
+      <section className="px-8 py-6 login-body rounded-b-lg">
+        {/* Email */}
+        <div className="flex flex-col mb-4">
+          <label htmlFor="email-enter" className="m-2 text-2xl">Email:</label>
+          <input type="email" name="email-enter" className="h-12 px-4 text-xl login-input" placeholder="Enter Email..."/>
+        </div>
+
+        {/* Password */}
+        <div className="flex flex-col mb-4">
+          <label htmlFor="password-enter" className="m-2 text-2xl">Password:</label>
+          <input type="password" name="password-enter" className="h-12 px-4 text-xl login-input" placeholder="Enter Password..."/>
+        </div>
+
+        {/* Submit and other links */}
+        <div className="flex flex-col w-full items-center">
+          <button type="submit" className="login-button mb-2">Login</button>
+          <Link href="/signup" className="text-gray-400 gray-link">Don&#39;t have an account yet? Sign up here!</Link>
+          <Link href="/password" className="text-gray-400 gray-link">Forgot Password?</Link>
+        </div>
+
+        {/* Google Auth */}
+        <div className="flex items-center my-4">
+          <div className="login-line"/>
+          <p className="login-line-text">Or</p>
+          <div className="login-line"/>
+        </div>
+
+        <button type="submit" className="h-16 p-4 mb-2 bg-sky-600">Login with Google</button>
+      </section>
+    </form>
+    </div>
+  )
+}
+
+export default Login
