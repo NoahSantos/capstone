3ec79c4336a729b4603a070b426fec034223a2cc
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-05-01 16:14:02 -0700

    MongoDB
    Working on connecting Oauth to MongoDB

---
+++ models/user.js
@@ -0,0 +1,17 @@
+import mongoose, {Schema, models} from 'mongoose';
+
+const userSchema = new Schema(
+    {
+        email:{
+            type: String,
+            required: true,
+        },
+        name:{
+            type: String,
+            required: true,
+        }
+    }, {timestamps:true}
+)
+
+const User = models.Students || mongoose.model('Students', userSchema);
+export default User;
