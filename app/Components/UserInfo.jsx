7d6b391c7664bfb9cd4c1ffa5772894df31bfd9f
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-05-01 15:12:04 -0700

    Oauth
    Basic google signing in is done. Just need to tie it to MongoDB

---
+++ app/Components/UserInfo.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+import SignInBtn from './SignInBtn';
+import {useSession} from 'next-auth/react'
+import Image from 'next/image'
+
+export default function UserInfo(){
+    const {status, data:session} = useSession();
+
+    if(status === 'authenticated'){
+        return (
+            <div className='shadow-xl p-8 rounded-md flex flex-col gap-3 bg-yellow-200'>
+                <Image width={60} height={60} src={session?.user?.image} className='rounded-full'></Image>
+                <div className='font-bold'>Name: <span>{session?.user?.name}</span></div>
+                <div className='font-bold'>Email: <span>{session?.user?.email}</span></div>
+            </div>
+        )
+    }else{
+        return (
+            <SignInBtn></SignInBtn>
+        )
+    }
+}
