35d8b5bc790998ea02abe6881e6af39a97eda1a6
Noah-Santos <noahbsantos@gmail.com>
2024-05-07 21:46:53 -0700

    Login
    Started on the login functionality, but it is very basic

--- app/api/user/route.js
+++
@@ -1,8 +0,0 @@
-import User from '@/server/models/user';
-import {NextResponse} from 'next/server';
-
-export async function POST(req){
-    const {name, email} = await req.json();
-    await User.create({name, email});
-    return NextResponse.json({message: 'User Registered'}, {status: 201});
-}
