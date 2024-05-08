55956d5ea2b8fa1d200232bfc7be2b8f86a46626
Noah-Santos <noahbsantos@gmail.com>
2024-05-07 21:48:13 -0700

    Login
    The previous commit removed an unnecessary file, and this commit is the basic login functionality that is still in progress

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+'use client';
+
+import React, {useRef} from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 const Login = () => {
+  let email = useRef(null);
+  let password = useRef(null);
+
+  let login = async () =>{
+    let users = await fetch('http://localhost:7000/users').then(response =>{
+      return response.json();
+    })
+
+    users.data.map(user=>{
+      if(user.email === email.current.value && user.password === password.current.value && user){
+
+      }
+    })
+  }
+
   return (
     <div className="flex justify-center login-background items-center">
     {/* Login Box */}
@@ -27,18 +45,18 @@ const Login = () => {
         {/* Email */}
         <div className="flex flex-col mb-4">
           <label htmlFor="email-enter" className="m-2 text-2xl">Email:</label>
-          <input type="email" name="email-enter" className="h-12 px-4 text-xl login-input" placeholder="Enter Email..."/>
+          <input type="email" name="email-enter" className="h-12 px-4 text-xl login-input" placeholder="Enter Email..." ref={email}/>
         </div>
 
         {/* Password */}
         <div className="flex flex-col mb-4">
           <label htmlFor="password-enter" className="m-2 text-2xl">Password:</label>
-          <input type="password" name="password-enter" className="h-12 px-4 text-xl login-input" placeholder="Enter Password..."/>
+          <input type="password" name="password-enter" className="h-12 px-4 text-xl login-input" placeholder="Enter Password..." ref={password}/>
         </div>
 
         {/* Submit and other links */}
         <div className="flex flex-col w-full items-center">
-          <button type="submit" className="login-button mb-4">Login</button>
+          <button type="submit" className="login-button mb-4" onClick={()=>login}>Login</button>
           <Link href="/signup" className="text-gray-400 gray-link mb-2">Don&#39;t have an account yet? Sign up here!</Link>
           <Link href="/password" className="text-gray-400 gray-link">Forgot Password?</Link>
         </div>
