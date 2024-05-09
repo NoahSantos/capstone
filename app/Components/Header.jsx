47170016fff87daf440ffa40c3d93af15ec60894
MercutioG <110576904+MercutioG@users.noreply.github.com>
2024-05-09 14:08:35 -0700

    Fixed weird header bug
    - Made the gap in the header dissapear
    - Made a middle ground when the header is not mobile view, but too small for normal desktop view

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className='sticky top-0 left-0 w-[90%] mt-7 -mb-7 h-0 z-50'>
       <div className='flex'>
-      <nav className='p-4 text-3xl text-white flex items-center justify-between w-full'>
+      <nav className='p-4 text-3xl text-white flex items-center justify-between w-full rounded-sm bg-[#F57F20]'>
         <Link href="/" className='flex'>
           <Image
             src="/vet-paw.png"
@@ -23,7 +23,7 @@ const Header = () => {
           <Link href='/login' className='pl-6'>Login</Link>
         </div>
       </nav>
-      <div className='w-0 h-0 
+      <div className='w-0 h-0 relative -left-[1px] triangle-nav
       border-t-[2.5rem] border-t-transparent
       border-l-[2.5rem] border-l-[#F57F20]
       border-b-[2.5rem] border-b-transparent'/>
