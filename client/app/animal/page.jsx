10f609219f5ac52b57425a1a6326ce571716b996
MercutioG <110576904+MercutioG@users.noreply.github.com>
2024-05-10 15:20:52 -0700

    Fixed nav
    - Fixed a few bugs with the mobile nav
    - Began improving login media query
    - Touched up animals page

--- client/app/animal/page.jsx
+++ client/app/animal/page.jsx
@@ -14,6 +14,7 @@ const AnimalPage = () => {
 
   return (
     <>
+    <Header/>
     <section className='animal-box p-8 my-8'>
       <p className='text-6xl m-4 font-medium animal-header'>{animal.name}, Age {animal.age}</p>
       <div className='flex animal-info'>
