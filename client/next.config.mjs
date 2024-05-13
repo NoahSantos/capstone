58a094760bd83edf8c27071bdc5e14ac1d689799
RenLevingar <110576801+RenLevingar@users.noreply.github.com>
2024-05-13 13:51:19 -0700

    Events Overhaul
    Updated the home page to get all the events from a component called events. This pulls from the data and then displays them based off of that. The link that is on each of the event cards takes you to pages of that specific event with the information pulled from the data.

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinifyMode: true,
   images: {
     remotePatterns: [
-      {hostname: 'res.cloudinary.com'},{hostname: 'cdn.pixabay.com'},{hostname: 'encrypted-tbn0.gstatic.com'}, {hostname: 'images.immediate.co.uk'}
+      {hostname: 'res.cloudinary.com'},{hostname: 'cdn.pixabay.com'},{hostname: 'encrypted-tbn0.gstatic.com'}, {hostname: 'images.immediate.co.uk'}, {hostname: 'images.squarespace-cdn.com'}, {hostname: 'arc.stimg.co'},
     ],
     domains: ['lh3.googleusercontent.com']
   }
