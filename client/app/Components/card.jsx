2a9358526afccc8509d089332819ed7299c82481
Noah-Santos <noahbsantos@gmail.com>
2024-05-14 16:10:45 -0700

    Cards
    Fixed the cards to show species and availability instead of numbers

--- client/app/Components/card.jsx
+++ client/app/Components/card.jsx
@@ -23,14 +23,14 @@ const AnimalCard = () => {
   return (
     <div className="animals-card-container flex flex-wrap justify-around">
       
-      {animals.map((item) => (
-        <Link href={{
+      {animals.map((item, id) => (
+        <Link key={id} href={{
           pathname: '/animal',
           query: { id: item.id },
         }}>
-        <div className="animals-card flex flex-col rounded">
-          <div className={`animals-card-${item.status}`}/>
-          <p className="animals-card-gender">{item.gender[0]}</p>
+        <div key={id} className="animals-card flex flex-col rounded">
+          {item.status === '0' ? <div className={`animals-card-green`}/> : item.status === '1' ? <div className={`animals-card-red`}/> : <div className={`animals-card-yellow`}/>}
+          <p className="animals-card-gender">{item.gender}</p>
           <Image
             src={item.profile}
             width={400}
@@ -42,7 +42,7 @@ const AnimalCard = () => {
           <div
           className='m-0 text-center animals-card-description rounded-b'>
             <h1 className='text-black text-2xl my-2 mx-4'>{item.name} : {item.age}</h1>
-            <h1 className='text-black text-base mb-3 mx-4'>{item.species}, Asian Semi-Longhair</h1>
+            <h1 className='text-black text-base mb-3 mx-4'>{item.species === '0' ? 'Dog' : "Cat"}, {item.breed}</h1>
           </div>
         </div>
       </Link>
