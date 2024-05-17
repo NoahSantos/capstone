f72c3ad2dedd02eca4aa8559d62e7023e0e5d1d6
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-05-17 14:15:26 -0700

    Even More Dependencies
    Removed even more dependencies

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const session = require ('express-session');
-// const flash = require('connect-flash');
 const morgan = require('morgan');
 const passport = require('passport');
 // tells where the passport LocalStrategy is
@@ -24,8 +23,6 @@ app.use("/events", eventRoute);
 
 // development tools
 app.use(morgan('tiny'));
-// EJS
-app.set('view engine', 'ejs');
 // body parser
 // determines the data format
 app.use(express.urlencoded({extended: false}));
@@ -38,15 +35,6 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-// use flash messaging---express
-// app.use(flash());
-// app.use((req, res, next) => {
-//     // creating the messages that we use in other files
-//     res.locals.success_msg = req.flash('sucess message');
-//     res.locals.error_msg = req.flash('error_msg');
-//     res.locals.error = req.flash('error');
-//     next()
-// });
 
 // connects the mongodb database to the project
 // this means that the connection is the first thing to happen
